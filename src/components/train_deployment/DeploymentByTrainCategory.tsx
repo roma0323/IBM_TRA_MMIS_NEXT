26110d1ced8f8215ebd8de4f3394e0b24951c898
Ray_Chen <ray@Ray-Chendebijixingdiannao.local>
2024-07-30 16:03:09 +0800

    Feat: taindeployment detail toggle func logic

--- src/components/train_deployment/DeploymentByTrainCategory.tsx
+++ src/components/train_deployment/DeploymentByTrainCategory.tsx
@@ -5,31 +5,33 @@ import SmallPieChart from "@/components/train_deployment/SmallPieChart";
 interface MyComponentProps {
   trainName: string;
   trainCount: number;
+  onClick: () => void;
 }
 
-const DeploymentByTrainCategory: React.FC<MyComponentProps> = ({ trainName, trainCount }) => {
+const DeploymentByTrainCategory: React.FC<MyComponentProps> = ({ trainName, trainCount, onClick }) => {
   const [isActive, setIsActive] = useState(false);
 
   const handleToggle = () => {
     setIsActive(prevState => !prevState);
+    onClick();
   };
 
   return (
     <div
-      className={"flex items-start gap-3 p-4 self-stretch w-full flex-[0_0_auto] bg-[#3034380d] rounded overflow-hidden  border-[#397EFF] border-l-4 flex-col justify-center relative"}
+      className="flex items-start gap-3 p-4 self-stretch w-full flex-[0_0_auto] bg-[#3034380d] rounded overflow-hidden border-[#397EFF] border-l-4 flex-col justify-center relative"
       onClick={handleToggle}
     >
-      <div className="relative w-fit mt-[-4.00px]">
+      <div className="text-lg">
         {trainName}
       </div>
-      <div className="flex items-center justify-between relative self-stretch w-full flex-[0_0_auto]">
-        <div className="inline-flex flex-col items-start relative flex-[0_0_auto] flex w-[150px] relative">
-        車輛數
-          <div>
-            {trainCount} 輛
+      <div className="flex items-center justify-between relative self-stretch w-full">
+        <div className="ml-2 inline-flex flex-col items-start relative flex relative">
+          <div className="text-4xl">
+            {trainCount}
           </div>
+          <div className="text-md">車輛數</div>
         </div>
-        <div className="relative w-[80px] h-[80px] inline-flex flex-col items-center justify-center relative flex-[0_0_auto]">
+        <div className="relative w-[80px] h-[80px] inline-flex flex-col items-center justify-center flex-[0_0_auto]">
           <SmallPieChart />
         </div>
       </div>
--- src/components/train_deployment/DetailClientPage.tsx
+++ src/components/train_deployment/DetailClientPage.tsx
@@ -1,14 +1,11 @@
-
 "use client";
 
-import React from "react";
+import React, { useState } from 'react';
 import { DataCard } from "@/components/train_deployment/DataCard";
 import BigPieChart from "@/components/train_deployment/BigPieChart";
 import DeploymentByTrainCategory from "@/components/train_deployment/DeploymentByTrainCategory";
-// import UseRateLineChart from "@/components/train_deployment/UseRateLineChart";
 import UseRateAreaChart from "@/components/train_deployment/UseRateAreaChart";
-import {DetailTrainByDepot} from "@/components/train_deployment/DetailTrainByDepot";
-
+import DetailTrainByDepot from "@/components/train_deployment/DetailTrainByDepot";
 
 const trainData = [
     { trainName: "太魯閣-TEMU1000", trainCount: 200 },
@@ -16,94 +13,85 @@ const trainData = [
     { trainName: "自強號-TEMU3000", trainCount: 120 },
     // Add more train data as needed
 ];
+
 type ClientPageProps = {
     initialData: any[];
 };
 
-
 const DetailClientPage: React.FC<ClientPageProps> = ({ initialData }) => {
+    const [selectedTrainName, setSelectedTrainName] = useState('');
+    const [isDetailVisible, setIsDetailVisible] = useState(false);
+
+    const handleTrainClick = (trainName: string) => {
+        if (trainName === selectedTrainName) {
+            setIsDetailVisible(!isDetailVisible);
+        } else {
+            setSelectedTrainName(trainName);
+            setIsDetailVisible(true);
+        }
+    };
+
     const cnt_sum = initialData.reduce((acc, item) => acc + item.current_cnt, 0);
     const ready_sum = initialData.reduce((acc, item) => acc + item.current_ready, 0);
+
     return (
-        <div className=" grid grid-cols-3 flex items-start gap-4 p-6 relative self-stretch flex-[0_0_auto] bg-neutral-100   ">
-
-            <div className="h-full inline-flex flex-col  items-start gap-4 relative flex-[0_0_auto]">
-
-                {/* 分配資訊   */}
+        <div className="grid grid-cols-3 flex items-start gap-4 p-6 relative self-stretch flex-[0_0_auto] bg-neutral-100">
+            <div className="h-full inline-flex flex-col items-start gap-4 relative flex-[0_0_auto]">
+                {/* 分配資訊 */}
                 <div className="w-full flex-[0_0_auto] rounded-lg flex flex-col items-start relative bg-white overflow-hidden">
-                    <div className="flex flex-col items-start justify-center p-2.5   w-full  border-b  border-[#646464]">
+                    <div className="flex flex-col items-start justify-center p-2.5 w-full border-b border-[#646464]">
                         分配資訊
                     </div>
                     <div className="flex flex-col items-start gap-[5px] px-2.5 py-0 relative self-stretch w-full flex-[0_0_auto]">
                         <div className="flex items-center gap-5 px-0 py-[5px] relative self-stretch w-full flex-[0_0_auto] rounded-[5px] overflow-hidden">
                             <DataCard text={cnt_sum} text1="可用數" />
                             <DataCard text={ready_sum} text1="總輛數" />
-                            <DataCard text={`${Math.round(ready_sum / cnt_sum * 100).toString()}%`} text1="可用率" />
+                            <DataCard text={`${Math.round((ready_sum / cnt_sum) * 100)}%`} text1="可用率" />
                         </div>
                     </div>
                 </div>
-                {/* 分配資訊   */}
-
-                {/* 近30 天使用率   */}
-                <div className="w-full self-stretch  flex-[0_0_auto] rounded-lg flex flex-col items-center relative bg-white overflow-hidden">
-                    <div className="flex flex-col items-start justify-center p-2.5   w-full  border-b  border-[#646464]">
+                {/* 近30 天使用率 */}
+                <div className="w-full self-stretch flex-[0_0_auto] rounded-lg flex flex-col items-center relative bg-white overflow-hidden">
+                    <div className="flex flex-col items-start justify-center p-2.5 w-full border-b border-[#646464]">
                         近30 天使用率
                     </div>
                     <UseRateAreaChart />
-
                 </div>
-                {/* 近30 天使用率   */}
-
-                {/* 近30 城際列車 - 車種配置   */}
-                <div className=" w-full h-full relative rounded-lg flex flex-col items-start justify-start  bg-white 	">
-
-                    <div className="flex flex-col items-start justify-center  p-2.5   w-full  border-b  border-[#646464]">
+                {/* 近30 城際列車 - 車種配置 */}
+                <div className="w-full h-full relative rounded-lg flex flex-col items-start justify-start bg-white">
+                    <div className="flex flex-col items-start justify-center p-2.5 w-full border-b border-[#646464]">
                         城際列車 - 車種配置
                     </div>
-                    <div className="relative w-full  flex items-start justify-center ">
+                    <div className="relative w-full flex items-start justify-center">
                         <BigPieChart />
                     </div>
                 </div>
-                {/* 近30 城際列車 - 車種配置   */}
             </div>
-
-
-
             <div className="h-full gap-2.5 flex-1 grow rounded-lg flex flex-col items-start relative bg-white overflow-hidden">
-
-                <div className="flex flex-col items-start justify-center p-2.5   w-full  border-b  border-[#646464]">
+                <div className="flex flex-col items-start justify-center p-2.5 w-full border-b border-[#646464]">
                     城際列車 - 車種分配資訊
                 </div>
-
-                <div className="overflow-auto flex flex-col items-start gap-2.5 px-3 py-2 relative  w-full h-[67vh]" >
+                <div className="overflow-auto flex flex-col items-start gap-2.5 px-3 py-2 relative w-full h-[67vh]">
                     {trainData.map((train, index) => (
                         <DeploymentByTrainCategory
                             key={index}
                             trainName={train.trainName}
                             trainCount={train.trainCount}
+                            onClick={() => handleTrainClick(train.trainName)}
                         />
                     ))}
                 </div>
             </div>
-
             <div className="h-full gap-2.5 flex-1 grow rounded-lg flex flex-col items-start relative bg-white overflow-hidden">
-
-                <div className="flex flex-col items-start justify-center p-2.5   w-full  border-b  border-[#646464] ">
-                    城際列車 - 機務段分配資訊</div>
-
-                    <div className="overflow-auto flex flex-col items-start gap-2.5 px-3 py-2 relative  w-full h-[67vh]" >
-                    <DetailTrainByDepot />
-                    <DetailTrainByDepot />
-                    <DetailTrainByDepot />
+                <div className="flex flex-col items-start justify-center p-2.5 w-full border-b border-[#646464]">
+                    城際列車 - 機務段分配資訊
                 </div>
-
-                    
+                <div className="overflow-auto flex flex-col items-start gap-2.5 px-3 py-2 relative w-full h-[67vh]">
+                    {isDetailVisible && <DetailTrainByDepot trainName={selectedTrainName} />}
+                </div>
             </div>
-
-
         </div>
     );
 };
-
 
 export default DetailClientPage;
