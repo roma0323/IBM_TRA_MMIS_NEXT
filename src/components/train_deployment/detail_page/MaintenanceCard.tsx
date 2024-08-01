c77075089aaad8a794fcaa172cd43306ce6d2fb5
Ray_Chen <ray@Ray-Chendebijixingdiannao.local>
2024-08-01 14:15:30 +0800

    fix: can not add 引號in div

--- src/components/train_deployment/detail_page/MaintenanceCard.tsx
+++ src/components/train_deployment/detail_page/MaintenanceCard.tsx
@@ -8,21 +8,21 @@ const MaintenanceCard: React.FC<DetailTrainByDepotProps> = ({ trainName }) => {
     return (
         <div className="flex items-start gap-3 p-4 self-stretch w-full flex-[0_0_auto] bg-[#3034380d] rounded overflow-hidden border-l-4 flex-col justify-center relative">
             <div className="relative w-fit mt-[-4.00px]">
-            車組"EMU503"-代號"64"
+            車組$$EMU503$$-代號$$64$$
             </div>
             <div className="flex items-center justify-between relative self-stretch w-full flex-[0_0_auto]">
                 <div className="inline-flex flex-col items-start w-full flex">
                     <div className="w-full py-1 px-6 flex items-center justify-between relative cursor-pointer transition-transform rounded-xl duration-300 ease-in-out transform hover:scale-105 hover:border hover:border-gray-200">
                         <div>動態說明</div>
-                        <div>1</div>
+                        <div>$$1$$</div>
                     </div>
                     <div className="w-full py-1 px-6 flex items-center justify-between relative cursor-pointer transition-transform rounded-xl duration-300 ease-in-out transform hover:scale-105 hover:border hover:border-gray-200">
                         <div>檢修級別</div>
-                        <div>1</div>
+                        <div>$$1$$</div>
                     </div>
                     <div className="w-full py-1 px-6 flex items-center justify-between relative cursor-pointer transition-transform rounded-xl duration-300 ease-in-out transform hover:scale-105 hover:border hover:border-gray-200">
                         <div>開工至完工日期</div>
-                        <div>1</div>
+                        <div>$$1$$</div>
                     </div>
                     
                 </div>
