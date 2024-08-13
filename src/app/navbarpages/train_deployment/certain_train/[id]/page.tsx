b9aa4931e3ecaad11e50e85ef9e82cc15a3e0093
Ray_Chen <ray@Ray-Chendebijixingdiannao.local>
2024-08-13 15:33:13 +0800

    chore: add suspense

--- src/app/navbarpages/train_deployment/certain_train/[id]/page.tsx
+++ src/app/navbarpages/train_deployment/certain_train/[id]/page.tsx
@@ -1,21 +1,21 @@
 import ClientPage from "@/components/train_deployment/detail_page/ClientPage";
 import { getSumStatusListAndCarcatalogEqualParam } from "@/api/api";
-
-interface Params {
-  id: string;
-}
+import React, { Suspense } from 'react';
+import Loading from "@/components/Loading"; // Import your custom Loading component
 
 interface PageProps {
   params: any;
 }
 
-export default async function Page({ params }:PageProps) {
+export default async function Page({ params }: PageProps) {
   const decodedLastPart = decodeURIComponent(params.id);
 
   const data = await getSumStatusListAndCarcatalogEqualParam(decodedLastPart);
   return (
     <main>
-      <ClientPage Data={data.data} /> 
+      <Suspense fallback={<Loading />}>
+        <ClientPage Data={data.data} />
+      </Suspense>
     </main>
   );
 }
