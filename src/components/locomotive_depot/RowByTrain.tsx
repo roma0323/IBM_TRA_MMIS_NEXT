4e924b8caa03df0f4ae12e281a15a265a6f90ec3
roma0323 <r911223oger@gmail.com>
2024-08-14 14:12:12 +0800

    fix: remove  click function in rowByTrain

--- src/components/locomotive_depot/RowByTrain.tsx
+++ src/components/locomotive_depot/RowByTrain.tsx
@@ -8,8 +8,8 @@ interface TrainData {
   trainData: TrainDataInArray;
   onDivClick: (dept: string, cartype: string, divName: string) => void; // Update callback prop
 }
-const numberBoxClassName="m-1 flex items-center justify-center cursor-pointer rounded-2xl hover:text-primary hover:shadow-md hover:shadow-primary/30 hover:scale-110"
-
+const CanClickClassName="m-1 flex items-center justify-center cursor-pointer rounded-2xl hover:text-primary hover:shadow-md hover:shadow-primary/30 hover:scale-110"
+const CannotClickClassName="my-1 flex items-center justify-center "
 export const RowByTrain = ({
   trainData,
   onDivClick,
@@ -25,22 +25,22 @@ export const RowByTrain = ({
           text={trainData.carcatalog}
         />
       </div>
-      <div className="my-1 flex items-center justify-center ">{trainData.deptdesc}</div>
-      <div className="my-1 flex items-center justify-center ">{trainData.cartype}</div>
-      <div className={numberBoxClassName}>{trainData.belongto}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("borrowout")}>{trainData.borrowout}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("current_temp")}>{trainData.current_temp}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("current_cnt")}>{trainData.current_cnt}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("current_use")}>{trainData.current_use}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("current_temp")}>{trainData.current_temp}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("current_ready")}>{trainData.current_ready}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("maintain_w")}>{trainData.maintain_w}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("maintain_sec")}>{trainData.maintain_sec}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("maintain_fac")}>{trainData.maintain_fac}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("oth_waitrep")}>{trainData.oth_waitrep}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("oth_return")}>{trainData.oth_return}</div>
-      <div className={numberBoxClassName} onClick={() => handleDivClick("oth_stop")}>{trainData.oth_stop}</div>
-      <div className={numberBoxClassName}>
+      <div className={CannotClickClassName}>{trainData.deptdesc}</div>
+      <div className={CannotClickClassName}>{trainData.cartype}</div>
+      <div className={CannotClickClassName}>{trainData.belongto}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("borrowout")}>{trainData.borrowout}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("current_temp")}>{trainData.current_temp}</div>
+      <div className={CannotClickClassName}>{trainData.current_cnt}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("current_use")}>{trainData.current_use}</div>
+      <div className={CannotClickClassName}>{trainData.current_temp}</div>
+      <div className={CannotClickClassName} >{trainData.current_ready}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("maintain_w")}>{trainData.maintain_w}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("maintain_sec")}>{trainData.maintain_sec}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("maintain_fac")}>{trainData.maintain_fac}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("oth_waitrep")}>{trainData.oth_waitrep}</div>
+      <div className={CanClickClassName} onClick={() => handleDivClick("oth_return")}>{trainData.oth_return}</div>
+      <div className={CannotClickClassName} >{trainData.oth_stop}</div>
+      <div className={CannotClickClassName}>
         {Number(trainData.availability.toFixed(2))}
       </div>
     </div>
