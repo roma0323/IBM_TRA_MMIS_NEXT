dbc01d4202dc935a08b6245a0bbcd5b5bf7e40c7
roma0323 <r911223oger@gmail.com>
2024-09-04 15:27:26 +0800

    feat:report daily complete

--- src/components/fault_notification/ClientPage.tsx
+++ src/components/fault_notification/ClientPage.tsx
@@ -1,76 +1,111 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { ReportLevelCard } from "@/components/fault_notification/ReportLevelCard";
-import ComposedChartMonthly from "@/components/factory_maintenance/ComposedChartMonthly";
-import ComposedChartAccmulate from "@/components/factory_maintenance/ComposedChartAccmulate";
-import { FailListItem } from "@/types/type";
-import { ReportLevelCardDataType } from "@/types/type";
 import BoardTitleSection from "@/components/BoardTitleSection";
-
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { FailListItem, ReportLevelCardDataType } from "@/types/type";
 
 type Props = {
   fail_list: FailListItem[];
-  ReportLevelCardData: ReportLevelCardDataType[]; // Add the 'ReportLevelCardData' property here
+  ReportLevelCardData: ReportLevelCardDataType[];
 };
 
+const headers = [
+  "車次",
+  "車型",
+  "車組車號",
+  "通報號",
+  "事故等級",
+  "通報時間",
+  "事故地點",
+  "TCMS故障代碼",
+  "故障位置",
+  "故障現象",
+  "狀態",
+  "責任單位",
+  "臨維單",
+];
+
 const ClientPage: React.FC<Props> = ({ fail_list, ReportLevelCardData }) => {
-  const [activeCardIndex, setActiveCardIndex] = useState<number | null>(null);
-  const [selectFactory, setSelectFactory] = useState<string>("");
+  const [activeCardIndex, setActiveCardIndex] = useState<number | null>(0);
+  const [selectFactory, setSelectFactory] = useState<string>("A");
+
+  const renderTableRows = (data: FailListItem[]) => {
+    return data.map((fail_by_row, index) => (
+      <TableRow key={index}>
+        <TableCell className="font-medium">{fail_by_row.trains_no}</TableCell>
+        <TableCell>{fail_by_row.cartype}</TableCell>
+        <TableCell>{fail_by_row.assetnum}</TableCell>
+        <TableCell>{fail_by_row.repnum}</TableCell>
+        <TableCell>{fail_by_row.fail_lvl}</TableCell>
+        <TableCell>{fail_by_row.reptime}</TableCell>
+        <TableCell>{fail_by_row.rep_loc}</TableCell>
+        <TableCell>{fail_by_row.tcms_code}</TableCell>
+        <TableCell>{fail_by_row.fail_loc}</TableCell>
+        <TableCell>{fail_by_row.fail_phenomenon}</TableCell>
+        <TableCell>{fail_by_row.fail_status}</TableCell>
+        <TableCell>{fail_by_row.fail_dept}</TableCell>
+        <TableCell className="text-right">{fail_by_row.fail_cmwo}</TableCell>
+      </TableRow>
+    ));
+  };
+
+  const filteredFailList =
+    selectFactory === "All"
+      ? fail_list
+      : fail_list.filter((fail_by_row) => selectFactory === fail_by_row.fail_lvl);
 
   return (
-    <div className="relative flex gap-6 p-6 h-full  ">
-
-        <div className="min-w-[35%] flex items-center justify-center">
-          <BoardTitleSection
-            title={`故障通報分級`}
-            content={
-              <div className="flex flex-col mx-4">
-                {ReportLevelCardData.map((CardDataByLevel, index) => (
-                  <ReportLevelCard
-                    key={index}
-                    CardDataByLevel={CardDataByLevel}
-                    isActive={activeCardIndex === index}
-                    onToggle={() => {
-                      setActiveCardIndex(
-                        activeCardIndex === index ? null : index
-                      );
-                      setSelectFactory(CardDataByLevel.fail_lvl + "等級");
-                    }}
-                  />
-                ))}
-              </div>
-            }
-          />
-        </div>
-
-        <div className="min-w-[63%] flex items-center justify-center">
-          <BoardTitleSection
-            title={`${selectFactory}-檢修走勢`}
-            content={
-              <div className="size-full">
-                {/* {Data.map(
-                  (chartData, index) =>
-                    activeCardIndex === index && (
-                      <div
-                        key={index}
-                        className=" size-full flex flex-col py-8 items-center justify-between"
-                      >
-                        <div className="flex justify-center items-center w-full">
-                          <ComposedChartMonthly data={chartData.monthData} />
-                        </div>
-                        <div className="flex justify-center items-center w-full">
-                          <ComposedChartAccmulate data={chartData.monthData} />
-                        </div>
-                      </div>
-                    )
-                )} */}
-              </div>
-            }
-          />
-        </div>
-
-       
+    <div className="relative flex justify-between p-6 gap-6 h-full">
+      <div className="min-w-[400px] flex items-center justify-center">
+        <BoardTitleSection
+          title={`故障通報分級`}
+          content={
+            <div className="flex flex-col mx-4">
+              {ReportLevelCardData.map((CardDataByLevel, index) => (
+                <ReportLevelCard
+                  key={index}
+                  CardDataByLevel={CardDataByLevel}
+                  isActive={activeCardIndex === index}
+                  onToggle={() => {
+                    setActiveCardIndex(index);
+                    setSelectFactory(CardDataByLevel.fail_lvl);
+                  }}
+                />
+              ))}
+            </div>
+          }
+        />
+      </div>
+      <div className="grow flex items-center justify-center">
+        <BoardTitleSection
+          title={`${selectFactory}級`}
+          content={
+            <div className="p-6">
+              <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                  <TableRow className="bg-slate-100 rounded-lg">
+                    {headers.map((header, index) => (
+                      <TableHead key={index}>{header}</TableHead>
+                    ))}
+                  </TableRow>
+                </TableHeader>
+                <TableBody>{renderTableRows(filteredFailList)}</TableBody>
+              </Table>
+            </div>
+          }
+        />
+      </div>
     </div>
   );
 };
--- src/components/fault_notification/ReportLevelCard.tsx
+++ src/components/fault_notification/ReportLevelCard.tsx
@@ -3,68 +3,64 @@ import { DataCard } from "@/components/train_deployment/DataCard";
 import { FailListItem } from "@/types/type";
 import { ReportLevelCardDataType } from "@/types/type";
 
-
-type Props ={
+type Props = {
   CardDataByLevel: ReportLevelCardDataType;
   isActive: boolean;
   onToggle: () => void;
-}
+};
 
 export const ReportLevelCard: React.FC<Props> = ({
   CardDataByLevel,
   isActive,
   onToggle,
 }) => {
-  const [isHovered, setIsHovered] = useState(false);
   const [clickedDiv, setClickedDiv] = useState<string | null>("allUnits");
-
-  const handleDivClick = (divName: string) => {
-    setClickedDiv((prev) => (prev === divName ? null : divName));
-  };
-
-  const handleOuterDivClick = () => {
-    onToggle();
-  };
 
   return (
     <div
-      className={`mt-5 p-5 rounded-lg flex-col inline-flex  transform ${
-        clickedDiv !== null ? "bg-slate-100" : ""
+      className={`mt-3 p-3 rounded-lg flex-col inline-flex  transform ${
+        isActive && clickedDiv !== null ? "bg-slate-100" : ""
       }`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onClick={handleOuterDivClick}
+      onClick={onToggle}
     >
       <div className="self-stretch items-start flex border-b border-[#646464]/20 justify-between items-center inline-flex">
         <div className="">
           <span className="text-9xl">{CardDataByLevel.fail_lvl}</span>
           <span className="ml-2 text-lg">級</span>
         </div>
-        <div className="flex flex-col grow items-end">
+        <div className="flex flex-col grow  items-end">
           <div
-            className={`cursor-pointer px-2 ${clickedDiv === "other" ? "bg-blue-100" : ""} hover:bg-blue-100`}
-            onClick={() => handleDivClick("other")}
+            className={`cursor-pointer px-2 rounded-lg   hover:border-l-4 border-primary/20 ${
+              isActive && clickedDiv === "other" ? "bg-white border-l-4 border-primary/20" : ""
+            }  `}
+            onClick={() => setClickedDiv("other")}
           >
             <span className="text-xs">其他</span>
-            <span className="text-4xl pl-4">{CardDataByLevel.other_fail_quantity}</span>
+            <span className="text-4xl pl-4">
+              {CardDataByLevel.other_fail_quantity}
+            </span>
           </div>
           <div
-            className={`cursor-pointer px-2 hover:bg-blue-100 ${
-              clickedDiv === "mechanical" ? "bg-blue-100" : ""
+            className={`cursor-pointer px-2 rounded-lg   hover:border-l-4 border-primary/20 ${
+              isActive && clickedDiv === "mechanical" ? "bg-white border-l-4 border-primary/20" : ""
             }`}
-            onClick={() => handleDivClick("mechanical")}
+            onClick={() => setClickedDiv("mechanical")}
           >
             <span className="text-xs">機務處</span>
-            <span className="text-4xl pl-4">{CardDataByLevel.Maintenance_fail_quantity}</span>
+            <span className="text-4xl pl-4">
+              {CardDataByLevel.Maintenance_fail_quantity}
+            </span>
           </div>
           <div
-            className={`cursor-pointer px-2 hover:bg-blue-100 ${
-              clickedDiv === "allUnits" ? "bg-blue-100" : ""
+            className={`cursor-pointer px-2 rounded-lg   hover:border-l-4 border-primary/20 ${
+              isActive && clickedDiv === "allUnits" ? "bg-white border-l-4 border-primary/20" : ""
             }`}
-            onClick={() => handleDivClick("allUnits")}
+            onClick={() => setClickedDiv("allUnits")}
           >
             <span className="text-xs">全部單位</span>
-            <span className="text-4xl pl-4">{CardDataByLevel.all_fail_quantity}</span>
+            <span className="text-4xl pl-4">
+              {CardDataByLevel.all_fail_quantity}
+            </span>
           </div>
         </div>
       </div>
