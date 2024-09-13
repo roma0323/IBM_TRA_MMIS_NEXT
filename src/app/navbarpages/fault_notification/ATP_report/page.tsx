21a4a1d743b7f71ec96092fb6ecedf5565e48260
roma0323 <r911223oger@gmail.com>
2024-09-13 15:49:55 +0800

    feat:ATP report basic layout

--- src/app/navbarpages/fault_notification/ATP_report/page.tsx
+++ src/app/navbarpages/fault_notification/ATP_report/page.tsx
@@ -6,8 +6,175 @@ export default async function Page({
 }: {
   searchParams: { date?: string; type?: string };
 }) {
-  
+  //http://192.168.36.21/maximo/zz_data?method=getATPFailYear&year=2024
+  const test = [
+    {
+      year: "2024",
+      month: "01",
+      failcnt: "26",
+      dailyfailcnt: "0.8400000000000001",
+    },
+    {
+      year: "2024",
+      month: "02",
+      failcnt: "2",
+      dailyfailcnt: "0.06999999999999999",
+    },
+    {
+      year: "2024",
+      month: "03",
+      failcnt: "3",
+      dailyfailcnt: "0.09999999999999999",
+    },
+    {
+      year: "2024",
+      month: "04",
+      failcnt: "3",
+      dailyfailcnt: "0.1",
+    },
+    {
+      year: "2024",
+      month: "05",
+      failcnt: "3",
+      dailyfailcnt: "0.09999999999999999",
+    },
+  ];
 
+  //http://192.168.36.21/maximo/zz_data?method=getATPFailList&year=2024&key=cartype
+  const cartype = [
+    {
+      key: "E500",
+      cnt: 2,
+      percentage: 3,
+    },
+    {
+      key: "EMU3000",
+      cnt: 7,
+      percentage: 12,
+    },
+    {
+      key: "TEMU1000",
+      cnt: 3,
+      percentage: 5,
+    },
+    {
+      key: "E1000",
+      cnt: 4,
+      percentage: 7,
+    },
+  ];
+
+  //http://192.168.36.21/maximo/zz_data?method=getATPFailList&year=2024&key=factor
+
+  const 故障要因分析 = [
+    {
+      key: "非ATP故障",
+      cnt: 1,
+      percentage: 4,
+    },
+    {
+      key: "外部干擾",
+      cnt: 1,
+      percentage: 4,
+    },
+    {
+      key: "設備故障",
+      cnt: 21,
+      percentage: 91,
+    },
+  ];
+
+  //URL: http://192.168.36.21/maximo/zz_data?method=getATPFailList&year=2024&key=element
+
+  const 故障設備分析 = [
+    {
+      key: "04.BTM感應子傳輸模組",
+      cnt: 4,
+      percentage: 17,
+      event: [
+        {
+          key: "CAU天線",
+          cnt: 3,
+          percentage: 75,
+        },
+        {
+          key: "BTM感應子傳輸模組",
+          cnt: 1,
+          percentage: 25,
+        },
+      ],
+    },
+    {
+      key: "非ATP故障",
+      cnt: 1,
+      percentage: 4,
+      event: [
+        {
+          key: "非ATP故障",
+          cnt: 1,
+          percentage: 100,
+        },
+      ],
+    },
+    {
+      key: "外部干擾",
+      cnt: 1,
+      percentage: 4,
+      event: [
+        {
+          key: "受干擾",
+          cnt: 1,
+          percentage: 100,
+        },
+      ],
+    },
+    {
+      key: "05.SDU速度和距離裝置",
+      cnt: 2,
+      percentage: 9,
+      event: [
+        {
+          key: "SDU2",
+          cnt: 1,
+          percentage: 50,
+        },
+        {
+          key: "SDU1",
+          cnt: 1,
+          percentage: 50,
+        },
+      ],
+    },
+  ];
+
+  //URL: http://192.168.36.21/maximo/zz_data?method=getATPFailList&year=2024&islist=1&key=cartype
+  const list = [
+    {
+      enterdate: "2024-09-03",
+      trainno: "2008",
+      assetnum: "ED9152",
+      assetgroup: "EMU900",
+      dept: "七堵機務段",
+      description: "ED9152號車上ATP故障",
+    },
+    {
+      enterdate: "2024-08-24",
+      trainno: "401",
+      assetnum: "TED1007",
+      assetgroup: "TEMU1000",
+      dept: "臺北機務段",
+      description:
+        "機車TED1007車上ATP異常,ATP一直跳地上感應子故障並緊急緊軔數次",
+    },
+    {
+      enterdate: "2024-08-17",
+      trainno: "4191",
+      assetnum: "EMC584",
+      assetgroup: "EMU500",
+      dept: "臺北機務段",
+      description: "電車EMC584號車上ATP故障",
+    },
+  ];
   return (
     <main className="grow bg-neutral-100 overflow-hidden relative">
       <ClientPage />
--- src/components/fault_notification/ATP_report/ClientPage.tsx
+++ src/components/fault_notification/ATP_report/ClientPage.tsx
@@ -1,78 +1,167 @@
 "use client";
 
 import React, { useState } from "react";
-import { ReportLevelCard } from "@/components/fault_notification/annual_report/ReportLevelCard";
 import BoardTitleSection from "@/components/BoardTitleSection";
-import GroupBarChart from "@/components/fault_notification/annual_report/GroupBarChart";
 import PieChart from "@/components/fault_notification/annual_report/PieChart";
 import { DataCard } from "@/components/train_deployment/DataCard";
-
-import { FailListItem, ReportLevelCardDataType } from "@/types/type";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const ClientPage: React.FC = () => {
-  const [activeCardIndex, setActiveCardIndex] = useState<number | null>(0);
-  const [selectFactory, setSelectFactory] = useState<string>("A");
-  const FakegetSumFailYearTypeAData = [
-    {
-      year: "2024",
-      month: "01",
-      kpicartype: "電力機車",
-      atp: "0",
-      duty: "0",
-      dutydept: "",
-      failtype: "A",
-    },
-    {
-      year: "2024",
-      month: "01",
-      kpicartype: "通勤列車",
-      atp: "0",
-      duty: "0",
-      dutydept: "",
-      failtype: "A",
-    },
+  const [selectMonth, setSelectMonth] = useState<string>("A");
+  const [selectedCellValue, setSelectedCellValue] = useState<string | null>(
+    null
+  );
+
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
   ];
 
+  const handleRowClick = (value: string) => {
+    setSelectedCellValue(value);
+  };
+
   return (
-    <div className=" grid grid-rows-4 grid-flow-col gap-4 p-4  h-full">
-      <div className="relative row-span-1  col-span-1   ">
+    <div className="grid grid-rows-4 grid-flow-col gap-4 p-4 h-full">
+      <div className="relative row-span-1 col-span-1">
         <BoardTitleSection
           title="總覽"
-          content={<div className="flex justify-around "><DataCard text="100" text1="故障通報" />
-                <DataCard text="100" text1="行車責任事故" /></div>}
+          content={
+            <div className="size-full flex justify-around items-center">
+              <DataCard text="100" text1="故障通報" />
+              <DataCard text="100" text1="行車責任事故" />
+            </div>
+          }
         />
       </div>
-      <div className="row-span-3  col-span-1   relative">
+      <div className="row-span-3 col-span-1 relative">
         <BoardTitleSection
           title="各月份故障"
-          content={<div className=" "> sdac</div>}
+          content={
+            <div className="mx-4">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>月份</TableHead>
+                    <TableHead>故障數</TableHead>
+                    <TableHead className="text-right">日均故障數</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow onClick={() => handleRowClick("01")}>
+                    <TableCell className="font-medium">01</TableCell>
+                    <TableCell>1</TableCell>
+                    <TableCell className="text-right">0.01</TableCell>
+                  </TableRow>
+                  <TableRow onClick={() => handleRowClick("02")}>
+                    <TableCell className="font-medium">02</TableCell>
+                    <TableCell>2</TableCell>
+                    <TableCell className="text-right">0.01</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+              {selectedCellValue && (
+                <div className="mt-4">Selected Month: {selectedCellValue}</div>
+              )}
+            </div>
+          }
         />
       </div>
 
-      <div className="row-span-2 col-span-3  grid grid-cols-3   gap-4 relative "> 
-
-        
-          <BoardTitleSection
-            title="故障車型分析"
-            content={<div className=" "> sdac</div>}
-          />
-          <BoardTitleSection
-            title="故障要因分析"
-            content={<div className=" "> sdac</div>}
-          />
-          <BoardTitleSection
-            title="故障設備分析"
-            content={<div className=" "> sdac</div>}
-          />
-        
-        
+      <div className="row-span-2 col-span-3 grid grid-cols-3 gap-4 relative">
+        <BoardTitleSection
+          title="故障車型分析"
+          content={
+            <div className="size-full">
+              <PieChart data={data} />
+            </div>
+          }
+        />
+        <BoardTitleSection
+          title="故障要因分析"
+          content={
+            <div className="size-full">
+              <PieChart data={data} />
+            </div>
+          }
+        />
+        <BoardTitleSection
+          title="故障設備分析"
+          content={
+            <div className=" size-full">
+              {" "}
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>月份</TableHead>
+                    <TableHead>故障數</TableHead>
+                    <TableHead className="text-right">日均故障數</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow onClick={() => handleRowClick("01")}>
+                    <TableCell className="font-medium">01</TableCell>
+                    <TableCell>1</TableCell>
+                    <TableCell className="text-right">0.01</TableCell>
+                  </TableRow>
+                  <TableRow onClick={() => handleRowClick("02")}>
+                    <TableCell className="font-medium">02</TableCell>
+                    <TableCell>2</TableCell>
+                    <TableCell className="text-right">0.01</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </div>
+          }
+        />
       </div>
 
-      <div className="row-span-2 col-span-3  relative">
-      <BoardTitleSection
-            title="各月份故障清單"
-            content={<div className=" "> sdac</div>}
-          />
+      <div className="row-span-2 col-span-3 relative">
+        <BoardTitleSection
+          title="各月份故障清單"
+          content={
+            <div className=" size-full">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>日期</TableHead>
+                    <TableHead>車次</TableHead>
+                    <TableHead>車型</TableHead>
+                    <TableHead>車號</TableHead>
+                    <TableHead>所屬段</TableHead>
+                    <TableHead className="text-right">故障原因</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow onClick={() => handleRowClick("01")}>
+                    <TableCell className="font-medium">2024-09-03</TableCell>
+                    <TableCell>2008</TableCell>
+                    <TableCell>EMU900</TableCell>
+                    <TableCell>ED9152</TableCell>
+                    <TableCell>七堵機務段</TableCell>
+                    <TableCell className="text-right">
+                      機車TED1007車上ATP異常,ATP一直跳地上感應子故障並緊急緊軔數次
+                    </TableCell>
+                  </TableRow>
+                  <TableRow onClick={() => handleRowClick("02")}>
+                    <TableCell className="font-medium">02</TableCell>
+                    <TableCell>2</TableCell>
+                    <TableCell className="text-right"></TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>{" "}
+            </div>
+          }
+        />
       </div>
     </div>
   );
