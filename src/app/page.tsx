6bdaaa59d15c66fc53598b316bc9ef3554c7b1d5
roma0323 <r911223oger@gmail.com>
2024-10-18 17:13:21 +0800

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         }}
       />
 
-      <div style={{ fontSize: "18vh", lineHeight: "1.2" }} className="bottom-[10vh]  left-[5vw]   absolute text-white">
+      <div style={{ fontSize: "18vh", lineHeight: "1.2" }} className="bottom-[6vh]  left-[4vw]   absolute text-slate-100">
         <div >MM I S</div>
         <div>績效</div>
         <div>管理指標</div>
