bca3fbf32ffbcbc9402ac582eb0edeaf1e676f9b
roma0323 <r911223oger@gmail.com>
2024-11-07 17:37:14 +0800

    fix:generateStaticParams in train delpoy certain train

--- src/app/navbarpages/train_deployment/certain_train/[id]/page.tsx
+++ src/app/navbarpages/train_deployment/certain_train/[id]/page.tsx
@@ -4,7 +4,18 @@ import React, { Suspense } from "react";
 interface PageProps {
   params: any;
 }
-
+export function generateStaticParams() {
+  return [
+    { id: "intercity_train", name: "城際列車" },
+    { id: "commuter_train", name: "通勤列車" },
+    { id: "electric_locomotive", name: "電力機車" },
+    { id: "diesel_electric_locomotive", name: "柴電機車" },
+    { id: "diesel_hydraulic_locomotive", name: "柴液機車" },
+    { id: "diesel_passenger_car", name: "柴油客車" },
+    { id: "passenger_car", name: "客車" },
+    { id: "freight_car", name: "貨車" },
+  ];
+}
 
 export default function Page({ params }: PageProps) {
   return (
